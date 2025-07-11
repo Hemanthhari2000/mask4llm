633ed4b39403f8ce774069345f0271a905c763c7
hemanthhari2000 <hemanthhari2000@gmail.com>
2025-07-11 15:50:31 +1000

    Add. integrate the logic with textual tui app

--- src/mask4llm/tui.py
+++ src/mask4llm/tui.py
@@ -1,6 +1,11 @@
+from rich.text import Text
 from textual.app import App
 from textual.containers import Container, Horizontal, Vertical
 from textual.widgets import Button, Footer, Header, Input, Label, TextArea
+
+from mask4llm.main import mask, unmask
+from mask4llm.util import display_dict_in_json_format, pipe_str
+from mask4llm.widgets.copy_on_enter_log import CopyOnEnterLog
 
 
 class MaskerApp(App):
@@ -38,6 +43,8 @@ class MaskerApp(App):
                         "Output (Enter or Press y to copy)",
                         id="id-output-log-label",
                     ),
+                    CopyOnEnterLog(id="id-output-log", highlight=True),
+                    id="id-output-log-container",
                 ),
             )
             yield Container(
@@ -45,10 +52,60 @@ class MaskerApp(App):
                     "Mask Map (Enter or Press y to copy)",
                     id="id-mask-map-label",
                 ),
+                CopyOnEnterLog(id="id-mask-map-log", highlight=True),
+                id="id-mask-map-log-container",
             )
         yield Footer()
 
-    def on_button_pressed(self, _: Button.Pressed): ...
+    def on_button_pressed(self, event: Button.Pressed):
+        input_text = self.query_one("#id-input-text", TextArea).text.strip()
+
+        input_patterns_str = self.query_one("#id-pattern-input", Input).value
+        input_patterns = pipe_str(input_patterns_str)
+
+        output_log = self.query_one("#id-output-log", CopyOnEnterLog)
+        mask_map_log = self.query_one("#id-mask-map-log", CopyOnEnterLog)
+
+        mask_map: dict[str, str] = dict()
+        if event.button.id == "id-mask-btn":
+            if not input_text and len(input_patterns) == 1:
+                self.notify(
+                    "Input | Pattern(s) [b]cannot be empty[/b].",
+                    title="Error",
+                    timeout=4,
+                    severity="error",
+                )
+                return
+
+            masked_text, new_map = mask(input_text, input_patterns)
+            mask_map.update(new_map)
+            _ = output_log.clear().write(masked_text)
+
+        elif event.button.id == "id-unmask-btn":
+            if not input_text:
+                self.notify(
+                    "Input [b]cannot be empty[/b].",
+                    title="Error",
+                    timeout=4,
+                    severity="error",
+                )
+                return
+            unmasked_text = unmask(input_text, mask_map)
+            _ = output_log.clear().write(unmasked_text)
+
+        _ = mask_map_log.clear()
+        display_dict_in_json_format(mask_map_log, mask_map)
+
+    async def on_copy_on_enter_log_copied(
+        self, message: CopyOnEnterLog.Copied
+    ):
+        copied_length = len(message.content)
+
+        feedback = Text(
+            f"\n✅ Copied {copied_length} characters to clipboard",
+            style="green",
+        )
+        self.notify(str(feedback), timeout=2, title="Copied!")
 
 
 def run_tui():
